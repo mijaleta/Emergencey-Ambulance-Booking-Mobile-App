6ee431f76649cf1e8a1446488c88ed825eff3a75
Gezagn Bekele Reggassa <wangeelichakeenya1@gmail.com>
2024-03-29 23:48:41 +0300

    Updated

---
+++ lib/pages/emergency_types.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: ListView(
+        padding: EdgeInsets.all(24),
+        // children: [SettingsGroup()],
+      ),
+    );
+  }
+}
