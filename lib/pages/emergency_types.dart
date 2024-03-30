8b768bf61bc95e611f4be73b6f1e4ebf6f4812df
Gezagn Bekele Reggassa <wangeelichakeenya1@gmail.com>
2024-03-30 14:27:23 +0300

    Updated

--- lib/pages/emergency_types.dart
+++ lib/pages/emergency_types.dart
@@ -1,37 +1,57 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+// import 'dart:js';
 
-class SettingsPage extends StatefulWidget {
-  const SettingsPage({super.key});
+// import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart';
+
+// class SettingsPage extends StatefulWidget {
+//   const SettingsPage({super.key});
+
+//   @override
+//   State<SettingsPage> createState() => _SettingsPageState();
+// }
+
+// class _SettingsPageState extends State<SettingsPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: ListView(
+//         padding: EdgeInsets.all(24),
+//         children: [
+//           SettingsGroup(title: 'GENERAL', children: <Widget>[
+//             buildLogout(),
+//             buildDeleteAccount(),
+//           ])
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// Widget buildLogout() => SimpleSettingsTile(
+//       title: 'Logout',
+//       subtitle: '',
+//       leading: IconWidget(icon: Icons.logout, color: Colors.black),
+//       onTap: () => Utils.showSnackBar(context, 'Clicked Logout'),
+//     );
+
+// Widget buildDeleteAccount() => SinpleSettingsTile(
+//   title:'Delete Account',
+//   subtitle: '',
+//   leading: IconWidget(icon:Icons.Delete, color: Colors.pink),
+//   onTap: () => Utils.showSnackBar(context, 'Clicked Delete'),
+// )
+import 'package:flutter/material.dart';
+
+class EmergencyTypes extends StatefulWidget {
+  const EmergencyTypes({super.key});
 
   @override
-  State<SettingsPage> createState() => _SettingsPageState();
+  State<EmergencyTypes> createState() => _EmergencyTypesState();
 }
 
-class _SettingsPageState extends State<SettingsPage> {
+class _EmergencyTypesState extends State<EmergencyTypes> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: ListView(
-        padding: EdgeInsets.all(24),
-        children: [
-          SettingsGroup(title: 'GENERAL', children: <Widget>[
-            buildLogout(),
-            buildDeleteAccount(),
-          ])
-        ],
-      ),
-    );
+    return const Placeholder();
   }
 }
-
-Widget buildLogout() => SimpleSettingsTile(
-      title: 'Logout',
-      subtitle: '',
-      leading: IconWidget(icon: Icons.logout, color: Colors.black),
-      onTap: () => Utils.showSnackBar(context, 'Clicked Logout'),
-    );
-
-Widget buildDeleteAccount() => SinpleSettingsTile(
-  
-)
--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,33 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     // Navigation logic here to navigate to the language selection page first
-    Future.delayed(const Duration(seconds: 7), () {
+    Future.delayed(const Duration(seconds: 20), () {
       Navigator.pushReplacementNamed(context, '/languageSelection');
     });
 
-    return const Scaffold(
-      backgroundColor: Colors.blue,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Ambulance Booking',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+    return Scaffold(
+      // backgroundColor: Colors.blue,
+      // appBar: AppBar(
+      //   backgroundColor: Colors.blue,
+      //   title: const Text(
+      //     'Welcome Back,',
+      //     style: TextStyle(color: Colors.white),
+      //   ),
+      // ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          // Background image widget
+          Image.asset(
+            'assets/splash.jpg',
+            fit: BoxFit.cover,
+            alignment: Alignment.center,
+          ),
+          // overflow: Overflow.visible,
+
+          // Your other widgets like logo, loading animation, etc.
+          // Example:
+          // Center(
+          //   child: CircularProgressIndicator(), // Loading animation
+          // ),
+          // LinearProgressIndicator(
+          //   backgroundColor: Colors.grey,
+          //   valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+          // )
+          // Shimmer.fromColors(
+          //   baseColor: Colors.grey[300],
+          //   highlightColor: Colors.grey[100],
+          //   child: Container(
+          //     width: 200.0,
+          //     height: 100.0,
+          //     color: Colors.white,
+          //   ),
+          // )
+          SizedBox(
+            width: 200.0,
+            height: 100.0,
+            child: Shimmer.fromColors(
+              baseColor: Colors.white,
+              highlightColor: Colors.blue,
+              child: const Center(
+                child: Text(
+                  'Emergency Ambulance Booking',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
+        // child: Image(
+        //   image: AssetImage('assets/splash.jpg'),
+        // ),
+        // mainAxisAlignment: MainAxisAlignment.center,
+        // children: [
+        // CircularProgressIndicator(
+        //   valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+        // ),
+        // SizedBox(height: 20),
+        // Text(
+        //   'Ambulance Booking',
+        //   style: TextStyle(
+        //     color: Colors.white,
+        //     fontSize: 24,
+        //     fontWeight: FontWeight.bold,
+        //   ),
+        // ),
+        // ],
       ),
     );
   }
