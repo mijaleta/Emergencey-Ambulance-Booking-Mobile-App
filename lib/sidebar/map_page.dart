2a5b38358debbf1f284d761b4387340e9fe2a4d7
Gezagn Bekele Reggassa <wangeelichakeenya1@gmail.com>
2024-04-22 23:14:00 +0300

    Map Integration to Driver page

--- lib/sidebar/map_page.dart
+++ lib/sidebar/map_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:gebetamap/gebetamap.dart'; // Import GebetaMap API
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -10,55 +11,70 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   MapController controller = MapController(
-    initPosition: GeoPoint(latitude: 47.4358055, longitude: 8.4737324),
+    // 7.784619644494936,36.72530388570615 - Yebu
+    initPosition: GeoPoint(latitude: 7.784619644494936, longitude: 36.72530388570615),
     areaLimit: BoundingBox(
-      east: 10.4922941,
-      north: 47.8084648,
-      south: 45.817995,
-      west: 5.9559113,
+      east: 37.602588571093925, //Eastern Longtude
+      north: 8.097725425956478,  //Northern Latitude
+      south: 7.157504950238041,  //Southern Latitude
+      west: 36.58595254315327,   //Western Longtude
     ),
   );
+  // or set manually init position
+  // final controller = MapController.withPosition(
+  //   initPosition: GeoPoint(
+  //     latitude: 7.784619644494936,
+  //     longitude: 36.72530388570615,
+  //   ),
+  // );
   @override
   Widget build(BuildContext context) {
-    return OSMFlutter(
-        controller: controller,
-        osmOption: OSMOption(
-          userTrackingOption: const UserTrackingOption(
-            enableTracking: true,
-            unFollowUser: false,
-          ),
-          zoomOption: const ZoomOption(
-            initZoom: 8,
-            minZoomLevel: 3,
-            maxZoomLevel: 19,
-            stepZoom: 1.0,
-          ),
-          userLocationMarker: UserLocationMaker(
-            personMarker: const MarkerIcon(
-              icon: Icon(
-                Icons.location_history_rounded,
-                color: Colors.red,
-                size: 48,
-              ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title:const Text('Tracking Patient Location', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),
+      ),
+      body: OSMFlutter(
+          controller: controller,
+          osmOption: OSMOption(
+            userTrackingOption: const UserTrackingOption(
+              enableTracking: true,
+              unFollowUser: false,
             ),
-            directionArrowMarker: const MarkerIcon(
-              icon: Icon(
-                Icons.double_arrow,
-                size: 48,
-              ),
+            zoomOption: const ZoomOption(
+              initZoom: 15,
+              minZoomLevel: 3,
+              maxZoomLevel: 19,
+              stepZoom: 1.0,
             ),
-          ),
-          roadConfiguration: const RoadOption(
-            roadColor: Colors.yellowAccent,
-          ),
-          markerOption: MarkerOption(
-              defaultMarker: const MarkerIcon(
+            userLocationMarker: UserLocationMaker(
+              personMarker: const MarkerIcon(
                 icon: Icon(
-                  Icons.person_pin_circle,
-                  color: Colors.blue,
-                  size: 56,
+                  Icons.location_history_rounded,
+                  color: Colors.red,
+                  size: 48,
                 ),
-              )),
-        ));
+              ),
+              directionArrowMarker: const MarkerIcon(
+                icon: Icon(
+                  Icons.double_arrow,
+                  size: 48,
+                ),
+              ),
+            ),
+            roadConfiguration: const RoadOption(
+              roadColor: Colors.yellowAccent,
+            ),
+            markerOption: MarkerOption(
+                defaultMarker: const MarkerIcon(
+                  icon: Icon(
+                    Icons.person_pin_circle,
+                    color: Colors.blue,
+                    size: 56,
+                  ),
+                )),
+          ),
+      ),
+    );
   }
 }
