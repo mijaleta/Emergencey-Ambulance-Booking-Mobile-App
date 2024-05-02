7998ba6021811b3aa177605dc6d6c720536c2e09
Gezagn Bekele Reggassa <wangeelichakeenya1@gmail.com>
2024-05-03 00:58:05 +0300

    Update

--- lib/sidebar/google_map_page.dart
+++ lib/sidebar/google_map_page.dart
@@ -2,15 +2,16 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:gebetamap/gebetamap.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 // import 'package:latlong2/latlong.dart' as latlang;
 import 'package:location/location.dart';
 
 // Replace with your actual GebetaMap API key
-const String apiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjNiYmNhM2M4LWYwNWQtNDZiNy04YTk4LWQ1YmEyMTkzY2YzYSJ9.0InfDuks2cAZnfel8Wq8ItetdKkaLUXu2RjInGLQ0Pg";
+// const String apiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjNiYmNhM2M4LWYwNWQtNDZiNy04YTk4LWQ1YmEyMTkzY2YzYSJ9.0InfDuks2cAZnfel8Wq8ItetdKkaLUXu2RjInGLQ0Pg";
 
 class GoogleMapPage extends StatefulWidget {
-  const GoogleMapPage({super.key});
+  const GoogleMapPage({Key? key}) : super(key: key);
 
   @override
   State<GoogleMapPage> createState() => _GoogleMapPageState();
@@ -18,18 +19,28 @@ class GoogleMapPage extends StatefulWidget {
 
 class _GoogleMapPageState extends State<GoogleMapPage> {
   final locationController = Location();
-  static const  googlePlex = LatLng(7.694687998729552, 36.816481597954756);
-  static const  mountainview = LatLng(7.7176078060398625, 36.80540921925276);
-  // 7.7176078060398625, 36.80540921925276 - Walda Amanuel
-  // 7.697165300922039, 36.8149151878519 - The first
-
   LatLng? currentPosition;
+  LatLng? destinationLocation;
 
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) async =>await fetchLocationUpdates());
+    WidgetsBinding.instance!.addPostFrameCallback((_) async {
+      await fetchLocationUpdates();
+    });
   }
+
+  Future<void> callGeoCode(String locationText) async {
+    String apiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjNiYmNhM2M4LWYwNWQtNDZiNy04YTk4LWQ1YmEyMTkzY2YzYSJ9.0InfDuks2cAZnfel8Wq8ItetdKkaLUXu2RjInGLQ0Pg"; // Your API Key
+    GebetaMapRequest gmr = new GebetaMapRequest();
+    ResponseData rs = await gmr.geocode(locationText, apiKey);
+    print(rs.data);
+  }
+// Define a method to handle text field value changes and call callGeoCode
+  void handleLocationChange(String value) async {
+    await callGeoCode(value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,51 +51,110 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
           style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
         ),
       ),
-      body: GoogleMap(
-        initialCameraPosition:const CameraPosition(
-          target:googlePlex,
-          zoom: 13,
-        ),
-        markers: {
-          const Marker(markerId: MarkerId('currentLocation'),
-            icon: BitmapDescriptor.defaultMarker,
-            position: googlePlex,
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(16.0),
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200, // Adjust the color as needed
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Enter patient location',
+                hintText: 'e.g., Latitude, Longitude',
+                prefixIcon: Icon(Icons.location_pin),
+                border: InputBorder.none, // Remove the border
+              ),
+              onChanged: handleLocationChange,
+            ),
           ),
-          const Marker(markerId: MarkerId('destinationLocation'),
-            icon: BitmapDescriptor.defaultMarker,
-            position: mountainview,
+
+          Expanded(
+            child: GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: currentPosition ?? LatLng(0.0, 0.0),
+                zoom: 13,
+              ),
+              markers: _buildMarkers(),
+              polylines: _buildPolylines(),
+            ),
           ),
-        },
+        ],
       ),
     );
+  }
+
+  Set<Marker> _buildMarkers() {
+    Set<Marker> markers = {};
+    if (currentPosition != null) {
+      markers.add(
+        Marker(
+          markerId: MarkerId('currentLocation'),
+          icon: BitmapDescriptor.defaultMarker,
+          position: currentPosition!,
+        ),
+      );
+    }
+    if (destinationLocation != null) {
+      markers.add(
+        Marker(
+          markerId: MarkerId('destinationLocation'),
+          icon: BitmapDescriptor.defaultMarker,
+          position: destinationLocation!,
+        ),
+      );
+    }
+    return markers;
+  }
+
+  Set<Polyline> _buildPolylines() {
+    Set<Polyline> polylines = {};
+    if (currentPosition != null && destinationLocation != null) {
+      polylines.add(
+        Polyline(
+          polylineId: PolylineId('route'),
+          color: Colors.blue,
+          width: 3,
+          points: [currentPosition!, destinationLocation!],
+        ),
+      );
+    }
+    return polylines;
   }
 
   Future<void> fetchLocationUpdates() async {
     bool serviceEnabled;
     PermissionStatus permissionGranted;
     serviceEnabled = await locationController.serviceEnabled();
-    if(serviceEnabled){
+    if (!serviceEnabled) {
       serviceEnabled = await locationController.requestService();
-    }
-    else{
+    } else {
       return;
     }
+
     permissionGranted = await locationController.hasPermission();
-    if(permissionGranted == PermissionStatus.denied){
+    if (permissionGranted == PermissionStatus.denied) {
       permissionGranted = await locationController.requestPermission();
-      if(permissionGranted != PermissionStatus.granted){
+      if (permissionGranted != PermissionStatus.granted) {
         return;
       }
     }
+
     locationController.onLocationChanged.listen((currentLocation) {
-      if (currentLocation.latitude != null && currentLocation.longitude != null&& currentLocation.accuracy! < 100) {
-        if (currentPosition == null || currentPosition != LatLng(currentLocation.latitude!, currentLocation.longitude!)) {
+      if (currentLocation.latitude != null &&
+          currentLocation.longitude != null &&
+          currentLocation.accuracy! < 100) {
+        if (currentPosition == null ||
+            currentPosition !=
+                LatLng(currentLocation.latitude!, currentLocation.longitude!)) {
           setState(() {
-            currentPosition = LatLng(currentLocation.latitude!, currentLocation.longitude!);
+            currentPosition =
+                LatLng(currentLocation.latitude!, currentLocation.longitude!);
           });
           print(currentPosition);
-        }
-        else{
+        } else {
           print('Current Position is not updating');
         }
       }
