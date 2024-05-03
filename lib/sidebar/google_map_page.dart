1b80ea378dcb408f05b41d9270cda72fcbe33d46
Gezagn Bekele Reggassa <wangeelichakeenya1@gmail.com>
2024-05-03 17:33:57 +0300

    Update

--- lib/sidebar/google_map_page.dart
+++ lib/sidebar/google_map_page.dart
@@ -4,7 +4,7 @@ import 'package:ambu_app/users/myInput.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map/plugin_api.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 
@@ -18,6 +18,8 @@ class GoogleMapPage extends StatefulWidget {
 class _GoogleMapPageState extends State<GoogleMapPage> {
   final start = TextEditingController();
   final end = TextEditingController();
+  bool isVisible = false;
+  List<LatLng> routpoints = [LatLng(9.352951154649515, 42.80603840681846)];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,11 +45,62 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
                   List<Location> start_l = await locationFromAddress(start.text);
                   List<Location> end_l = await locationFromAddress(end.text);
 
-                  print(start.text);
-                  print(end.text);
+                  // print(start.text);
+                  // print(end.text);
+                  // print(start_l);
+                  // print(end_l);
+
+                  var v1 =start_l[0].latitude;
+                  var v2 =start_l[0].longitude;
+                  var v3 =end_l[0].latitude;
+                  var v4 =end_l[0].longitude;
+                  
+                  
+                  var url = Uri.parse('http://router.project-osrm.org/route/v1/driving/$v2,$v1;$v4,$v3?steps=true&annotations=true&geometries=geojson');
+
+                  var response = await http.get(url);
+
+                  setState(() {
+                    routpoints = [];
+
+                    var ruter = jsonDecode(response.body)['routes'][0]['geometry']['coordinates'];
+                    for(int i= 0; i< ruter.length; i++){
+                      var reep = ruter[i].toString();
+                      reep = reep.replaceAll("[","");
+                      reep = reep.replaceAll("]","");
+
+                      var lat1 = reep.split(',');
+                      var long1 = reep.split(",");
+
+                      routpoints.add(LatLng(double.parse(lat1[1]), double.parse(long1[1])));
+                    }
+                    // print(routpoints);
+                    isVisible = !isVisible;
+                  });
+                  // print(response.body);
+                  // print(start_l);
+                  // print(v1);
+                  // print(v2);
+                    
+                    
                   },
-                  child: Text('Press to Track...')
+                  child: Text('Press to Track...'),
+              ),
+              SizedBox(height: 10,),
+              FlutterMap(
+                options: MapOptions(
+                  center: routpoints[0],
+                  zoom: 14,
+                ),
+                children: [  // Use children for regular layers
+                  // Add your tile layers and other map elements here
+                ],
+                // Use nonRotatedChildren specifically for attribution (assuming it's available)
+                nonRotatedChildren: [
+
+                ],
               )
+
             ],
           ),
         ),
