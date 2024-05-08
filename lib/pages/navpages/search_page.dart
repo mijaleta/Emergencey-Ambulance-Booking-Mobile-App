8cba619cbe346c018c4dd7f8ba2d8b94e2e5ed77
Ethiopia12345 <101978426+mijaleta@users.noreply.github.com>
2024-05-08 20:28:21 +0300

    all

--- lib/pages/navpages/search_page.dart
+++ lib/pages/navpages/search_page.dart
@@ -1,55 +1,11 @@
-<<<<<<< Updated upstream
-import 'package:flutter/material.dart';
-// import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
-import 'package:url_launcher/url_launcher.dart';
-=======
 // import 'package:flutter/material.dart';
 // import 'package:flutter_map/flutter_map.dart';
 // import 'package:latlong2/latlong.dart';
 // import 'package:url_launcher/url_launcher.dart';
->>>>>>> Stashed changes
 
 // class SearchPage extends StatelessWidget {
 //   const SearchPage({Key? key});
 
-<<<<<<< Updated upstream
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Leaflet Map')),
-      // body: FlutterMap(
-      //   options: MapOptions(
-      //     initialCenter: LatLng(51.509364, -0.128928),
-      //     initialZoom: 9.2,
-      //   ),
-      //   children: [
-      //     TileLayer(
-      //       urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-      //       userAgentPackageName: 'com.example.app',
-      //     ),
-      //     MarkerLayer(markers: [
-      //       Marker(
-      //         width: 80.0,
-      //           height: 80.0,
-      //           point: LatLng(51.5, -0.09),
-      //           child: Icon(Icons.location_city_rounded,size: 50,),
-      //       ),
-      //     ]),
-      //     RichAttributionWidget(
-      //       attributions: [
-      //         TextSourceAttribution(
-      //           'OpenStreetMap contributors',
-      //           onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
-      //         ),
-      //       ],
-      //     ),
-      //   ],
-      // ),
-    );
-  }
-}
-=======
 //   @override
 //   Widget build(BuildContext context) {
 //     return Scaffold(
@@ -85,5 +41,4 @@ import 'package:url_launcher/url_launcher.dart';
 //     );
 //   }
 // }
->>>>>>> Stashed changes
 
